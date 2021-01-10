27f77a88372e9e088f0f27e36459a1c236a72b6b
Libbybacon <libbybacon@github.com>
2021-01-10 18:52:30 -0500

    First JavaScript project
    creating basic alerts and expressions, commenting code

---
+++ JavaScript/Basic_JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+window.alert("Hello, World!");
+document.write("Hello, World!");
+
+
+var Dog = "  Phoneix is my dog.  "
+document.write(Dog);
+
+var Name = "My name is Libby"
+window.alert(Name);
+
+var quote = "I don\'t like it when people sing the \"happy birthday\" song to me.  "
+document.write(quote);
+
+document.write("This is me concatenating" + " a string" + " made of several strings...");
+
+var concatenate = " Two strings " + "added together. "
+document.write(concatenate);
+
+var Family = "The Bacons", Dad = "Bink", Mom = "Edie", Daughter = "Libby", Son = "Cap";
+var Family = Family.fontcolor("green"), Dad=Dad.fontcolor("blue"), Mom=Mom.fontcolor("orange"), Daughter=Daughter.fontcolor("purple"), Son=Son.fontcolor("red");
+document.write(Mom);
+
+document.write(8*7);
