ebc7170a0901e7cd0c12e0c148e7d4c65b5b44d6
Libbybacon <libbybacon@github.com>
2021-01-10 19:12:46 -0500

    adding comments
    adding comments to all of code

--- JavaScript/Basic_JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript/Basic_JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,23 +1,29 @@
-window.alert("Hello, World!");
-document.write("Hello, World!");
+window.alert("Hello, World!"); //Displaying string in alert box
+document.write("Hello, World!"); //Displaying a string in the document
 
 
-var Dog = "  Phoneix is my dog.  "
-document.write(Dog);
+var Dog = "  Phoneix is my dog.  " //Defining a variable and assigning it a string value
+document.write(Dog); //Displaying a string in the document
 
-var Name = "My name is Libby"
-window.alert(Name);
+var Name = "My name is Libby" //Defining a variable and assigning it a string value
+window.alert(Name); //Displaying a string in an alert box
 
-var quote = "I don\'t like it when people sing the \"happy birthday\" song to me.  "
-document.write(quote);
+var quote = "I don\'t like it when people sing the \"happy birthday\" song to me.  " //Defining a variable and assigning it a string values;
+                                                                                    //Using \ to escape characters
+document.write(quote); //displaying a string in the document
 
-document.write("This is me concatenating" + " a string" + " made of several strings...");
+document.write("This is me concatenating" + " a string" + " made of several strings..."); //Displaying a concatenated string in the document
 
-var concatenate = " Two strings " + "added together. "
-document.write(concatenate);
+var concatenate = " Two strings " + "added together. " //Defining a variable and assigning it a value of concatenated strings
+document.write(concatenate); //Displaying a string in the document
 
-var Family = "The Bacons", Dad = "Bink", Mom = "Edie", Daughter = "Libby", Son = "Cap";
-var Family = Family.fontcolor("green"), Dad=Dad.fontcolor("blue"), Mom=Mom.fontcolor("orange"), Daughter=Daughter.fontcolor("purple"), Son=Son.fontcolor("red");
-document.write(Mom);
+var Family = "The Bacons", Dad = "Bink", Mom = "Edie", //Defining and assigning string values to multiple variables
+    Daughter = "Libby", Son = "Cap";
 
-document.write(8*7);
+var Family = Family.fontcolor("green"), Dad=Dad.fontcolor("blue"), //Using the str.fontcolor method to assign colors to variables
+    Mom=Mom.fontcolor("orange"), Daughter=Daughter.fontcolor("purple"), 
+    Son=Son.fontcolor("red");
+
+document.write(Mom); //Displaying a string in the document
+
+document.write(8*7); //Displaying the result of an expression in the document
