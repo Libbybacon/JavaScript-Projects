fa95652ddba21e47d97619ca2909321cd9910814
Libby Bacon <Libbybacon@gmail.com>
2021-05-06 13:18:40 -0400

    Delete JavaScript Projects/JavaScript_3 directory

--- JavaScript Projects/JavaScript_3/JavaScript_3.js
+++
@@ -1,6 +0,0 @@
-function showAuthor(author) {
-    var authorName = author.getAttribute("data-author-name");
-    alert(authorName + " wrote the book " + author.innerHTML);
-}
-
-//Not quite sure exactly how this works...
